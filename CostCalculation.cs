a4ff435e0d7855b129f6eaa265c2f0b51cca9fd4
sami9290 <sami9290@gmail.com>
2017-08-10 23:10:43 +0200

    zu

---
+++ CostCalculation.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Excel= Microsoft.Office.Interop;
+namespace TDD
+{
+    public class CostCalculation
+    {
+        Excel.Excel.Range currentFind ;
+        Excel.Excel.Range firstFind ;
+        Excel.Excel.Application xlApp;
+        Excel.Excel.Workbook xlWorkBook;
+        Excel.Excel.Worksheet xlWorkSheetFirst;
+        Excel.Excel.Worksheet xlWorkSheetSecond;
+        Excel.Excel.Worksheet xlWorkSheetThird;
+        public string strExcelFilePath= @"C:\Users\Sami\Desktop\TDD\IntakeAndRelease.xlsx";
+        public object misValue = System.Reflection.Missing.Value;
+        public Tuple<Excel.Excel.Application, Excel.Excel.Workbook, Excel.Excel.Worksheet, Excel.Excel.Worksheet, Excel.Excel.Worksheet> opendatasource()
+        {
+                xlApp = new Excel.Excel.Application();
+                xlWorkBook = xlApp.Workbooks.Open(strExcelFilePath
+                    , 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+                xlWorkSheetFirst = (Excel.Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+                xlWorkSheetSecond = (Excel.Excel.Worksheet)xlWorkBook.Worksheets.get_Item(2);
+                xlWorkSheetThird = (Excel.Excel.Worksheet)xlWorkBook.Worksheets.get_Item(3);
+          return  Tuple.Create(xlApp, xlWorkBook, xlWorkSheetFirst, xlWorkSheetSecond, xlWorkSheetThird);
+        }
+        public Tuple<DateTime, DateTime, int, int , string> cal10euroeachday(DateTime dtstartdate, DateTime dtenddate, int intPersonID )
+        {
+            TimeSpan t = dtenddate - dtstartdate;
+            double NrOfDays = t.TotalDays;
+            int intAmout = Convert.ToInt32(NrOfDays) * 10;
+            if (intAmout > 228) { intAmout = 228; }
+            var result = Tuple.Create(dtstartdate, dtenddate, intPersonID, intAmout, "Partial Payment");
+            return result;
+        }
+        public Tuple<DateTime, DateTime, int, int, string, bool> calservicefees(DateTime dtstartdate, DateTime dtenddate, int intPersonID, string strServiceName, Excel.Excel.Worksheet xlWorkSheetSecond, Excel.Excel.Worksheet  xlWorkSheetThird)
+        {
+            TimeSpan t = dtenddate - dtstartdate;
+            double NrOfDays = t.TotalDays;
+            int cellRow = 0;
+            string strInsurance=null;
+            int x = 0;
+            for (x = 1; x <= 9; x++)
+            {
+                if (xlWorkSheetSecond.Cells[x,1].text==(strServiceName))
+                {
+                    cellRow = x;
+                    break;
+                }
+            }
+           
+            int intAmout = Convert.ToInt32(NrOfDays) * Convert.ToInt32(xlWorkSheetSecond.Cells[cellRow, 2].Value.ToString());
+            for(int p=1; p < 27; p++)
+            {
+                if (xlWorkSheetThird.Cells[p , 1].text==(intPersonID.ToString()))
+                {
+                    strInsurance = xlWorkSheetThird.Cells[ p,7].text;
+                   
+                    break;
+                }
+            }
+            if (strInsurance == "No Insurance")
+            {
+                intAmout += (Convert.ToInt32(NrOfDays) * 10);
+                var result = Tuple.Create(dtstartdate, dtenddate, intPersonID, intAmout, "Individually paid", false);
+                return result;
+            }
+            else {
+                var result = Tuple.Create(dtstartdate, dtenddate, intPersonID, intAmout, strInsurance, true);
+                return result;
+            } 
+        }
+        public  Tuple<DateTime, DateTime, int, bool> CalPaymentPerPerson(int intPersonID , Excel.Excel.Worksheet xlWorkSheetFirst, Excel.Excel.Worksheet xlWorkSheetSecond, Excel.Excel.Worksheet xlWorkSheetThird)
+        {
+            DateTime dtStart ;
+            DateTime dtEnd ;
+            string strServiceName = null;
+            currentFind = null;
+            firstFind = null;
+            bool blCheckInsurance = true;
+            var result = Tuple.Create(DateTime.MinValue, DateTime.MinValue, intPersonID, blCheckInsurance);
+            Excel.Excel.Range Fruits = xlWorkSheetFirst.get_Range("A1", "A42");
+            // Specify all these parameters every time you call this method,
+            // since they can be overridden in the user interface. 
+            currentFind = Fruits.Find(intPersonID, misValue,
+                Excel.Excel.XlFindLookIn.xlValues, Excel.Excel.XlLookAt.xlPart,
+                Excel.Excel.XlSearchOrder.xlByRows, Excel.Excel.XlSearchDirection.xlNext, false,
+                misValue, misValue);
+                
+            while (currentFind != null)
+            {
+                string strRowNumber = currentFind.get_Address();
+                strRowNumber = strRowNumber.Substring(strRowNumber.LastIndexOf('$') + 1);
+                dtStart = Convert.ToDateTime(xlWorkSheetFirst.Cells[Convert.ToInt32(strRowNumber), 6].Value.ToString());
+                dtEnd = Convert.ToDateTime(xlWorkSheetFirst.Cells[Convert.ToInt32(strRowNumber), 7].Value.ToString());
+                strServiceName = xlWorkSheetFirst.Cells[Convert.ToInt32(strRowNumber), 3].Value.ToString();
+                Tuple<DateTime, DateTime, int, int, string, bool> userData = calservicefees(dtStart, dtEnd, intPersonID, strServiceName ,xlWorkSheetSecond , xlWorkSheetThird );
+                blCheckInsurance = userData.Item6;
+                return  result = Tuple.Create(dtStart, dtEnd, intPersonID, blCheckInsurance);
+                //Check if person has insurance
+                if (blCheckInsurance == true)
+                {
+                    cal10euroeachday(dtStart, dtEnd, intPersonID);
+                    return result = Tuple.Create(dtStart, dtEnd, intPersonID, blCheckInsurance);
+                }
+                // Keep track of the first range found. 
+                if (firstFind == null)
+                {
+                    firstFind = currentFind;
+                }
+                // If didn't move to a new range, done.
+                else if (currentFind.get_Address(Excel.Excel.XlReferenceStyle.xlA1)
+                        == firstFind.get_Address(Excel.Excel.XlReferenceStyle.xlA1))
+                {
+                    break;
+                }
+                currentFind = Fruits.FindNext(currentFind);
+            }
+            return result;
+        }
+    }
+}
---
+++ TestCases.cs
@@ -0,0 +1,239 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop;
+namespace TDD
+{
+    [TestFixture]
+    class TestCases
+    {
+        //gfd
+        //test rrt
+        Excel.Excel.Application xlApp;
+        Excel.Excel.Workbook xlWorkBook;
+        Excel.Excel.Worksheet xlWorkSheetFirst;
+        Excel.Excel.Worksheet xlWorkSheetSecond;
+        Excel.Excel.Worksheet xlWorkSheetThird;
+        public void opendatasource()
+        {
+            CostCalculation cc = new CostCalculation();
+            cc.opendatasource();
+            Tuple<Excel.Excel.Application, Excel.Excel.Workbook, Excel.Excel.Worksheet,
+                Excel.Excel.Worksheet, Excel.Excel.Worksheet> getData = cc.opendatasource();
+            xlApp = getData.Item1;
+            xlWorkBook = getData.Item2;
+            xlWorkSheetFirst = getData.Item3;
+            xlWorkSheetSecond = getData.Item4;
+            xlWorkSheetThird = getData.Item5;
+        }
+
+        [TestCase]
+        public void TC01()
+        {
+            CostCalculation cc = new CostCalculation();
+            cc.opendatasource();
+            Tuple<Excel.Excel.Application, Excel.Excel.Workbook, Excel.Excel.Worksheet,
+                Excel.Excel.Worksheet, Excel.Excel.Worksheet> getData = cc.opendatasource();
+            xlApp = getData.Item1;
+            xlWorkBook = getData.Item2;
+            xlWorkSheetFirst = getData.Item3;
+            xlWorkSheetSecond = getData.Item4;
+            xlWorkSheetThird = getData.Item5;
+        }
+
+        [TestCase]
+        public void TC02()
+        {
+            var TupleCompare = new Tuple<DateTime, DateTime, int, int, string>(
+                        Convert.ToDateTime("4/1/2015"), Convert.ToDateTime("4/9/2015"),
+                        4, 90, "Partial Payment");
+
+            CostCalculation cc = new CostCalculation();
+            Assert.AreEqual(TupleCompare, cc.cal10euroeachday(Convert.ToDateTime(4 / 1 / 2015),
+                Convert.ToDateTime( 4 / 9 / 2015),4));
+        }
+        [TestCase]
+        public void TC03()
+        {
+            var TupleCompare = new Tuple<DateTime, DateTime, int, int, string>(
+                      Convert.ToDateTime("4/1/2015"), Convert.ToDateTime("4/9/2015"),
+                      4, 80, "Partial Payment");
+
+            CostCalculation cc = new CostCalculation();
+            Assert.AreEqual(TupleCompare, cc.cal10euroeachday(Convert.ToDateTime("4/1/2015"),
+                Convert.ToDateTime("4/9/2015"), 4));
+        }
+        [TestCase]
+        public void TC04()
+        {
+            var TupleCompare = new Tuple<DateTime, DateTime, int, int, string>(
+               DateTime.Now, DateTime.Now.AddDays(2), 4, 20, "Partial Payment");
+
+            CostCalculation cc = new CostCalculation();
+            Assert.AreEqual(TupleCompare, cc.cal10euroeachday(DateTime.Now,
+                DateTime.Now.AddDays(2), 4));
+        }
+
+        [TestCase]
+        public void TC05()
+        {
+            var TupleCompare = new Tuple<DateTime, DateTime, int, int, string>(
+               DateTime.Now, DateTime.Now.AddDays(35), 4, 350, "Partial Payment");
+
+            CostCalculation cc = new CostCalculation();
+            Assert.AreEqual(TupleCompare, cc.cal10euroeachday(DateTime.Now,
+                DateTime.Now.AddDays(35), 4));
+        }
+
+        [TestCase]
+        public void TC06()
+        {
+            opendatasource();
+            var TupleCompare = new Tuple<DateTime, DateTime, int, int, string, bool>(
+            Convert.ToDateTime("4 / 1 / 2015"), Convert.ToDateTime("4 / 9 / 2015"), 4, 80, "TK", true);
+
+            CostCalculation cc = new CostCalculation();
+            Assert.AreEqual(TupleCompare, cc.calservicefees(Convert.ToDateTime("4 / 1 / 2015"),
+                Convert.ToDateTime("4 / 9 / 2015"), 4, "Emergency Department", xlWorkSheetSecond, xlWorkSheetThird));
+            xlWorkBook.Close(false);
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlWorkBook);
+        }
+  
+        [TestCase]
+        public void TC08()
+        {
+            opendatasource();
+            var TupleCompare = new Tuple<DateTime, DateTime, int, int, string, bool>(
+     Convert.ToDateTime("1/1/2015"), Convert.ToDateTime("8/1/2015"), 12, 6996, "Individually paid", false);
+
+            CostCalculation cc = new CostCalculation();
+            Assert.AreEqual(TupleCompare, cc.calservicefees(Convert.ToDateTime("1/1/2015"),
+                Convert.ToDateTime("8/1/2015"), 12, "Cataract surgery outcome", xlWorkSheetSecond, xlWorkSheetThird));
+            xlWorkBook.Close();
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlWorkBook);
+        }
+        [TestCase]
+        public void TC09()
+        {
+            opendatasource();
+            var TupleCompare = new Tuple<DateTime, DateTime, int, int, string, bool>(
+     Convert.ToDateTime("12/1/2015"), Convert.ToDateTime("12/12/2015"), 
+     10, 440, "AXA", true);
+
+            CostCalculation cc = new CostCalculation();
+            Assert.AreEqual(TupleCompare, cc.calservicefees(Convert.ToDateTime("12/1/2015"),
+                Convert.ToDateTime("12/12/2015"), 10, "Preventive Care", xlWorkSheetSecond, xlWorkSheetThird));
+            xlWorkBook.Close();
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlWorkBook);
+        }
+        [TestCase]
+        public void TC10()
+        {
+            opendatasource();
+            var TupleCompare = new Tuple<DateTime, DateTime,int, bool>(
+                         Convert.ToDateTime("4 / 1 / 2015"),
+                         Convert.ToDateTime("4 / 9 / 2015"), 4, false);
+
+            CostCalculation cc = new CostCalculation();
+            Assert.AreEqual(TupleCompare, cc.CalPaymentPerPerson( 4 ,
+                xlWorkSheetFirst, xlWorkSheetSecond, xlWorkSheetThird));
+            xlWorkBook.Close();
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlWorkBook);
+        }
+        [TestCase]
+        public void TC11()
+        {
+            opendatasource();
+            var TupleCompare = new Tuple<DateTime, DateTime, int, bool>(
+                         Convert.ToDateTime("11/19/2015"),
+                         Convert.ToDateTime("12/1/2015"), 4, true);
+
+            CostCalculation cc = new CostCalculation();
+            Assert.AreEqual(TupleCompare, cc.CalPaymentPerPerson(4,
+                xlWorkSheetFirst, xlWorkSheetSecond, xlWorkSheetThird));
+            xlWorkBook.Close();
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlWorkBook);
+        }
+        	
+         [TestCase]
+        public void TC12()
+        {
+            opendatasource();
+            var TupleCompare = new Tuple<DateTime, DateTime, int, bool>(
+                         Convert.ToDateTime("1/1/2015"),
+                         Convert.ToDateTime("8/1/2015"), 12, false);
+
+            CostCalculation cc = new CostCalculation();
+            Assert.AreEqual(TupleCompare, cc.CalPaymentPerPerson(12,
+                xlWorkSheetFirst, xlWorkSheetSecond, xlWorkSheetThird));
+            xlWorkBook.Close();
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlWorkBook);
+        }
+
+
+        [TestCase]
+        public void TC13()
+        {
+            opendatasource();
+            var TupleCompare = new Tuple<DateTime, DateTime, int, bool>(
+                         Convert.ToDateTime("1/1/2015"), 
+                         Convert.ToDateTime("8/1/2015"), 12, false);
+
+            CostCalculation cc = new CostCalculation();
+            Assert.AreEqual(TupleCompare, cc.CalPaymentPerPerson(12,
+                xlWorkSheetFirst, xlWorkSheetSecond, xlWorkSheetThird));
+
+            var TupleCompareServiceFees = new Tuple<DateTime, DateTime, int, int, string, bool>(
+      Convert.ToDateTime("1/1/2015"), Convert.ToDateTime("8/1/2015"),
+      12, 6996, "Individually paid", false);
+            
+            Assert.AreEqual(TupleCompareServiceFees,
+                cc.calservicefees(Convert.ToDateTime("1/1/2015"), Convert.ToDateTime("8/1/2015"),
+                12, "Cataract surgery outcome", xlWorkSheetSecond, xlWorkSheetThird));
+            xlWorkBook.Close();
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlWorkBook);
+        }
+
+        [TestCase]
+        public void TC14()
+        {
+            opendatasource();
+            var TupleCompare = new Tuple<DateTime, DateTime, int, bool>(
+                        Convert.ToDateTime("4/1/2015"), Convert.ToDateTime("4/9/2015"),
+                        4, true);
+
+            CostCalculation cc = new CostCalculation();
+            Assert.AreEqual(TupleCompare, cc.CalPaymentPerPerson(4, xlWorkSheetFirst,
+                xlWorkSheetSecond, xlWorkSheetThird));
+
+            var TupleCompareServiceFees = new Tuple<DateTime, DateTime,
+                int, int, string, bool>(
+      Convert.ToDateTime("4/1/2015"), Convert.ToDateTime("4/9/2015"),
+      4, 80, "TK", true);
+
+            Assert.AreEqual(TupleCompareServiceFees, 
+                cc.calservicefees(Convert.ToDateTime("4/1/2015"), Convert.ToDateTime("4/9/2015"),
+                4, "Emergency Department", xlWorkSheetSecond, xlWorkSheetThird));
+            var TupleCompare10Europerday = new Tuple<DateTime, DateTime, int, int, string>(
+                      Convert.ToDateTime("4/1/2015"), Convert.ToDateTime("4/9/2015"),
+                      4, 80, "Partial Payment");
+
+            Assert.AreEqual(TupleCompare10Europerday, cc.cal10euroeachday(Convert.ToDateTime("4/1/2015"),
+                Convert.ToDateTime("4/9/2015"), 4));
+            xlWorkBook.Close();
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlWorkBook);
+        }
+    }
+}
